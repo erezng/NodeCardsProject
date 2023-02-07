128c471593c6cafdfef7523d8521fdba7db8ee2e
erez gabay <erez2g@gmail.com>
2023-02-07 09:36:47 +0200

    save each user with arole of user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,25 +1,51 @@
 import { Router } from "express";
 import _ from "underscore";
 import { User } from "../db/models/user.js";
+import { validateSignUp } from "../middleware/verifySignInBody.js";
 import { userAlreadyExists } from "../middleware/userAlreadyExists.js";
-import { validateSignUp } from "../middleware/verifySignupBody.js";
 
+import bcrypt from "bcryptjs";
+import { Role } from "../db/models/role.js";
 const router = Router();
 
 //api/auth/signup
 router.post("/signup", validateSignUp, userAlreadyExists, async (req, res) => {
   const body = _.pick(req.body, "username", "email", "password");
 
+  //12 rounds takes more
+  body.password = await bcrypt.hash(body.password, 12);
+  //save the password hash to db:
+  const user = new User(body);
   try {
-    //find ({email})
-
-    //TODO: email and username already exsists
-
-    const user = await new User(body).save();
+    user.roles = [await (await Role.findOne({ name: "user" }))._id];
     return res.json({ message: "user saved", id: user._id });
   } catch (e) {
     return res.status(500).json({ message: "Server DB Error", error: e });
   }
+});
+
+router.post("/signin", validateSignUp, async (req, res) => {
+  //email and password:
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(401).json({ message: "No Such User" });
+    }
+
+    //123*12
+    //verify body.password matches user.password
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    return res.status(200).json({ message: "Sign in succesfull" });
+  } catch (e) {}
 });
 
 export { router as authRouter };
